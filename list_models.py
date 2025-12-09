9edc953dd3dbacc5388ba48382c934b2a6068c17
Udbhaw08 <udbhawinsta@gmail.com>
2025-12-09 13:35:12 +0530

    Initial commit

--- list_models.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyAGM6vwEUdqj9AeIWA7N0qTyuMJuy9VJRU")
-
-try:
-    for m in client.models.list():
-        print(m.name)
-except Exception as e:
-    print(e)
